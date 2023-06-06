44e417690e3e758b2d778a8e9588aa5bc425ffaa
ws-malvagonzale\malvagonzalez <malvagonzalez@proxya.com>
2023-06-06 21:37:12 +0200

    first commit

---
+++ src/app/persona.model.ts
@@ -0,0 +1,14 @@
+export class Persona{
+    /*
+    nombre: string ='';
+    apellido: string= '';
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    (abajo ponemos la sintáxis simplificada de esto)
+    */
+
+    constructor(public nombre:string, public apellido: string){}
+}
